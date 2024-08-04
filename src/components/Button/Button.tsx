b04cb90b02571c7d7e3e18aa9204a18c033e8300
Coriano Harris <corianoharrispro@gmail.com>
2024-08-04 12:15:17 -0700

    updated button component styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ const Button: React.FC<ButtonProps> = ({ children, isLoading, ...props }) =>
     return (
         <button
             {...props}
-            className={`w-48 p-2 rounded shadow-lg shadow-indigo-500/40 ${isLoading ? 'bg-gray-400' : 'bg-black hover:bg-slate-700'
+            className={`w-full p-4 rounded shadow-lg shadow-indigo-500/40 ${isLoading ? 'bg-gray-400' : 'bg-black hover:bg-slate-700'
                 } text-white ${props.className || ''}`}
             disabled={isLoading || props.disabled}
             
