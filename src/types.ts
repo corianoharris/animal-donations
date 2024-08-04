70cc7a7c361f19cd7e68889fc9291a8956d7cecd
Coriano Harris <corianoharrispro@gmail.com>
2024-08-04 12:13:31 -0700

    added transcation and transcation table props

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,20 @@
 export type FormInputsProps =
-{
-    animal: string,
-    zoo: string,
-    amount: string
-}
+    {
+        animal: string,
+        zoo: string,
+        amount: string
+    }
+
+export type TransactionProps =
+    {
+        animal: string
+        zoo: string
+        amount: number
+        date: string
+    }
+
+export type TransactionHistoryTableProps =
+    {
+        transactions: TransactionProps[],
+        showNoHistoryMessage: boolean,
+    }
