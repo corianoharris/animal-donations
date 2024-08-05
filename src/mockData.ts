6dd42ebd8b163663f8148d913cb7869e26f0ed94
Coriano Harris <corianoharrispro@gmail.com>
2024-08-05 10:39:04 -0700

    added mockData

---
+++ src/mockData.ts
@@ -0,0 +1,21 @@
+export const animalsList = [
+    { value: 'Cheetahs', label: 'Cheetahs' },
+    { value: 'Black Caiman', label: 'Black Caiman' },
+    { value: 'Blue Dragon', label: 'Blue Dragon' },
+    { value: 'Komondo Dragon', label: 'Komondo Dragon' },
+  ]
+  
+  export const zoosList = [
+    { value: 'San Diego Zoo', label: 'San Diego Zoo' },
+    { value: 'Memphis Zoo', label: 'Memphis Zoo' },
+    { value: 'Seattle Zoo', label: 'Seattle Zoo' },
+    { value: 'Mississippi Zoo', label: 'Mississippi Zoo' },
+  ]
+  
+  export const donationAmountsList = [
+    { value: '10', label: '$10' },
+    { value: '20', label: '$20' },
+    { value: '40', label: '$40' },
+    { value: '50', label: '$50' },
+    { value: '100', label: '$100' },
+  ]
